064fc8bf9b1d471637460eacd27549bd0a225460
code-with-saad <saadkashif770@gmail.com>
2022-10-30 02:53:26 +0500

    Assignment 3

---
+++ main.dart
@@ -0,0 +1,119 @@
+// Assignment 3
+
+void main() {
+// Task 1
+
+  // List names = ["Saad", "Ahmed", "Ali", "Rafay", "Salman"];
+  // print(names[0]);
+  // print(names[1]);
+  // print(names[2]);
+  // print(names[3]);
+  // print(names[4]);
+
+// x-------------x-------------x
+
+// Task 2
+
+  // List<String> days = [];
+  // days.add("Monday");
+  // days.add("Tuesday");
+  // days.add("Wednesday");
+  // days.add("Thursday");
+  // days.add("Friday");
+  // days.add("Saturday");
+  // days.add("Sunday");
+  // print(days);
+
+// x-------------x-------------x
+
+// Task 3
+
+  // List friends = [
+  //   "Saad",
+  //   "Amir",
+  //   "Hammad",
+  //   "Mustufa",
+  //   "Ahsan",
+  //   "Rafay",
+  // ];
+
+  // var friendname = friends.where((friend) {
+  //   return friend.startsWith("A");
+  // });
+  // print(friendname);
+
+// x-------------x-------------x
+
+// Task 4
+
+  // Map infor = {
+  // "name": "Saad",
+  // "address": "Defence Phase X Karachi",
+  // "age": 15,
+  // "country": "Dubai" };
+  // infor["country"] = "Pakistan";
+  // print(infor);
+
+// x-------------x-------------x
+
+// Task 5
+
+// Map names = {"name_1":"Saad",
+// "name_2" : "Rafay",
+// "name_3": "Arslan",
+// "name_4": "Ali",
+// "name_5": "Ahmed"};
+// names.removeWhere((key,value) => value.length > 4);
+// print("The Length 4 Element is: $names");
+
+// x-------------x-------------x
+
+// Task 6
+
+  // var World = {};
+  // World["Countries"] = {
+  //   "Country": "Pakistan",
+  //   "Capital City": "Islamabad",
+  //   "Currency": "Pakistani Rupee",
+  //   "Language": "Urdu"
+  // };
+
+  // World["Countries:"] = {
+  //   "Country": "Saudi Arabia",
+  //   "Capital City": "Riyadh",
+  //   "Currency": "Saudi riyal",
+  //   "Language": "Arabic"
+  // };
+  // World.forEach((key, val) {
+  //   print(' $key, $val');
+  // });
+
+// x-------------x-------------x
+
+// Task 7
+
+// Map<String, int> mathsMarks = {
+//   "ram": 30,
+//   "mark": 32,
+//   "harry": 88,
+//   "raj": 69,
+//   "john": 15,
+// };
+
+//   mathsMarks.removeWhere((key, value) => value <= 30);
+//   print(mathsMarks);
+
+// x-------------x-------------x
+
+// Task 8
+
+// Map<String, double> expenses = {
+//  "sun": 3000.0,
+//  "mon": 3000.0,
+//  "tue": 3234.0,
+// };
+  // expenses.putIfAbsent("fri", () => 5000.0);
+  // print(expenses);
+
+// x-------------x-------------x
+}
